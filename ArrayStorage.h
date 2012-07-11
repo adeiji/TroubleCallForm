ea45fe839cdc1007c869ed2494f32d6a87d8d68b
Developer <Developer@Jeffrey-Najarros-MacBook-Pro.local>
2012-07-11 11:26:02 -0700

    this is the program right before all the different memory fixes

---
+++ ArrayStorage.h
@@ -0,0 +1,16 @@
+//
+//  ArrayStorage.h
+//  Trouble Call Form
+//
+//  Created by Developer on 5/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ArrayStorage : NSObject
+extern NSMutableArray *myHoistSrlNumbers;
+extern NSMutableArray *myJobNumbers;
+extern NSMutableArray *myDates;
+
+@end
---
+++ ArrayStorage.m
@@ -0,0 +1,14 @@
+//
+//  ArrayStorage.m
+//  Trouble Call Form
+//
+//  Created by Developer on 5/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ArrayStorage.h"
+
+@implementation ArrayStorage
+
+
+@end
